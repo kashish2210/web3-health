b559df61ee010b77f6f1ec3fb39fa60bc87f9a6b
kashish2210 <153910815+kashish2210@users.noreply.github.com>
2024-09-21 00:00:07 +0530

    done

--- healthchain/settings.py
+++ healthchain/settings.py
@@ -95,6 +95,7 @@ TEMPLATES = [
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 WSGI_APPLICATION = 'healthchain.wsgi.application'
 # Security settings for HTTPS
@@ -162,6 +163,7 @@ MEDIA_URL = '/media/'
 
 # Define the location where media files are stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
