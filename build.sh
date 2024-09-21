f18494d95346a51edf7d21b90b165cd3d61f2dc1
kashish2210 <153910815+kashish2210@users.noreply.github.com>
2024-09-21 11:30:32 +0530

    lets see

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Exit on error
+@echo off
+echo Building project...
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
+gunicorn healthchain.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT --log-level=debug
